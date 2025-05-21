fed12aa7d9f2dc4e94c9095bdb370df882a88514
YujiKamiji <yujikamiji@gmail.com>
2025-05-21 13:53:52 -0300

    testbench finalizada?

--- Banc_ULA/banc_reg/banc_reg_16b_tb.vhd
+++ Banc_ULA/banc_reg/banc_reg_16b_tb.vhd
@@ -2,47 +2,65 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
-entity banc_reg_16b_tb is
-end banc_reg_16b_tb;
+entity banc_ula_tb is
+end banc_ula_tb;
 
-architecture Behavioral of banc_reg_16b_tb is
-    --sinais para o teste, depende doq tiver no banco de registradores
-    signal clk_tb      : std_logic;
-    signal rst_tb      : std_logic;
-    signal wr_en_tb    : std_logic;
-    signal wr_addr_tb  : std_logic_vector(3 downto 0);
-    signal wr_data_tb  : std_logic_vector(15 downto 0);
-    signal rd_addr_tb  : std_logic_vector(3 downto 0);
-    signal rd_data_tb  : std_logic_vector(15 downto 0);
-    signal finished    : std_logic := '0';
+architecture Behavioral of banc_ula_tb is
+    --sinais para o teste, depende doq tiver no Banc_ULA
+    signal clk_tb               : std_logic;
+    signal rst_tb               : std_logic := '0';
+    signal op_code_tb           : unsigned(1 downto 0);
+    signal reg_selector_tb      : unsigned(3 downto 0);
+    signal load_control_banco_tb: std_logic;
+    signal load_control_ac_tb   : unsigned(1 downto 0);
+    signal cmpi_control_tb      : std_logic;
+    signal load_value_tb        : unsigned(15 downto 0);
+    signal wr_reg_enable_tb     : std_logic;
+    signal flag_zero_tb         : std_logic;
+    signal flag_carry_tb        : std_logic;
+    signal reg_read_out_tb      : unsigned(15 downto 0);
+    signal ac_value_tb          : unsigned(15 downto 0);
+    signal finished             : std_logic := '0';
 
     constant period_time : time := 100 ns;
 
-    --o banco de registradores em si (VAI MUDAR)
-    component banc_reg_16b is
-        port(
-            clk      : in  std_logic;
-            rst      : in  std_logic;
-            wr_en    : in  std_logic;
-            wr_addr  : in  std_logic_vector(3 downto 0);
-            wr_data  : in  std_logic_vector(15 downto 0);
-            rd_addr  : in  std_logic_vector(3 downto 0);
-            rd_data  : out std_logic_vector(15 downto 0)
+    --componente a ser testado
+    component Banc_ULA is
+        Port (
+            clk: in std_logic;
+            op_code: in unsigned(1 downto 0);          
+            reg_selector: in unsigned(3 downto 0); 
+            load_control_banco: in std_logic;
+            load_control_ac: in unsigned(1 downto 0);
+            cmpi_control: in std_logic;
+            load_value: in unsigned(15 downto 0);     
+            wr_reg_enable: in std_logic;
+            reset: in std_logic;                                    
+            flag_zero: out std_logic;
+            flag_carry: out std_logic;
+            reg_read_out: out unsigned(15 downto 0); 
+            ac_value: out unsigned(15 downto 0)                      
         );
     end component;
 
 begin 
 
-    --instancia do banco com o port map (VAI MUDAR)
-    uut: banc_reg_16b
+    --instancia do componente a ser testado
+    Banc_ULA: Banc_ULA
         port map(
-            clk      => clk_tb,
-            rst      => rst_tb,
-            wr_en    => wr_en_tb,
-            wr_addr  => wr_addr_tb,
-            wr_data  => wr_data_tb,
-            rd_addr  => rd_addr_tb,
-            rd_data  => rd_data_tb
+            clk                  => clk_tb,
+            op_code              => op_code_tb,
+            reg_selector         => reg_selector_tb,
+            load_control_banco   => load_control_banco_tb,
+            load_control_ac      => load_control_ac_tb,
+            cmpi_control         => cmpi_control_tb,
+            load_value           => load_value_tb,
+            wr_reg_enable        => wr_reg_enable_tb,
+            reset                => rst_tb,
+            flag_zero            => flag_zero_tb,
+            flag_carry           => flag_carry_tb,
+            reg_read_out         => reg_read_out_tb,
+            ac_value             => ac_value_tb
         );
 
     --gera o clock so quando o teste estiver ativo (finished = '0')
@@ -76,13 +94,40 @@ begin
 
     process
     begin
-        wait for 200 ns; --espera o reset passar
+        wait for 300 ns; --espera o reset passar
 
         --testes vao vir aqui 
-        
 
+        -- carrega valor 10 no registrador 2
+        load_value_tb        <= to_unsigned(10, 16);
+        load_control_banco_tb <= '1'; -- usar load_value
+        reg_selector_tb      <= "0010";
+        wr_reg_enable_tb     <= '1';
+        wait until rising_edge(clk_tb);
 
-        wait;  -- OBRIGATÓRIO
+        -- carrega valor 5 no acumulador (pela entrada direta)
+        load_value_tb        <= to_unsigned(5, 16);
+        load_control_ac_tb   <= "01"; -- usar load_value
+        wait until rising_edge(clk_tb);
+
+        -- soma acumulador + registrador 2 (ac = 5 + 10)
+        wr_reg_enable_tb     <= '0'
+        op_code_tb           <= "00";  -- opcode para ADD
+        load_control_ac_tb   <= "00";  -- usar saida da ULA
+        reg_selector_tb      <= "0010"; -- registrador 2
+        cmpi_control_tb      <= '0';   -- usar valor do banco na ULA
+        wait until rising_edge(clk_tb);
+
+        -- grava valor do acumulador no registrador 3
+        load_control_banco_tb <= '0';  -- usar ac como entrada
+        wr_reg_enable_tb     <= '1';
+        reg_selector_tb      <= "0011"; -- registrador 3
+        wait until rising_edge(clk_tb);
+
+        -- desativa escrita
+        wr_reg_enable_tb     <= '0';
+        wait;
+
     end process;
 
 end Behavioral;
